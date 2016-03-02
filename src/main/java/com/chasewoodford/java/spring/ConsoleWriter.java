87ae5be6d1bcd7c7b760f9d532e04cb3ba94b00b
Chase Woodford <chase@chasewoodford.com>
2016-03-01 21:47:31 -0500

    autowiring with component-scan

--- src/main/java/com/chasewoodford/java/spring/ConsoleWriter.java
+++ src/main/java/com/chasewoodford/java/spring/ConsoleWriter.java
@@ -1,5 +1,8 @@
 package com.chasewoodford.java.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConsoleWriter implements LogWriter {
 
     public void write(String text) {
