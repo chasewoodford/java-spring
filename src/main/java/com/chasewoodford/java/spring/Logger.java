d32c011bc8cd153780dcc43593f66c01b00a562e
Chase Woodford <chase@chasewoodford.com>
2016-03-01 21:39:14 -0500

    autowiring annotations

--- src/main/java/com/chasewoodford/java/spring/Logger.java
+++ src/main/java/com/chasewoodford/java/spring/Logger.java
@@ -1,24 +1,29 @@
 package com.chasewoodford.java.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
 
     private LogWriter consoleWriter;
     private LogWriter fileWriter;
 
-    public void setConsoleWriter(LogWriter writer) {
-        this.consoleWriter = writer;
+    // @Autowired
+    public void setConsoleWriter(ConsoleWriter consoleWriter) {
+        this.consoleWriter = consoleWriter;
     }
 
-    public void setFileWriter(LogWriter fileWriter) {
-        this.fileWriter = fileWriter;
-    }
+    @Autowired
+    public void setFileWriter(LogWriter fileWriter) { this.fileWriter = fileWriter; }
 
     public void writeFile(String text) {
         fileWriter.write(text);
     }
 
     public void writeConsole(String text) {
-        consoleWriter.write(text);
+
+        if(consoleWriter != null)
+            consoleWriter.write(text);
+
     }
 
 }
